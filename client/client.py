a3221fcb3a765cddd885c50d7aa893c539409729
Irwanda <irwandaa7@gmail.com>
2022-10-08 12:01:03 +0700

    feat: RC4's encryption (client side only)

--- client/client.py
+++ client/client.py
@@ -10,6 +10,7 @@ from Crypto.Util.Padding import pad, unpad
 from Crypto.Cipher import DES, AES
 from collections import namedtuple
 import yaml
+import numpy as np
 
 
 def load_key():
@@ -56,6 +57,53 @@ def encrypt(src_path, key, AES_MODE=AES.MODE_ECB):
 
     return iv
 
+def encryptRC4(src_path, key):
+    with open(src_path, "r") as file:
+        file_data = file.read()
+
+    S = KSA(key)
+    encrypted_data = np.array(PRGA(S, len(file_data)))
+    dst_path = f"{cfg.ABSOLUTEPATH}/client/encrypted/{cfg.TARGET_FILE}".replace('.txt', '.bin')
+
+    # print(encrypted_data)
+    plaintext = np.array([ord(c) for c in file_data])
+    cipher = plaintext ^ encrypted_data
+
+    # print(cipher.astype(np.uint8).data.hex())
+    # print([chr(c) for c in cipher])
+
+    final_encrypted = "".join([chr(c) for c in cipher])
+    with open(dst_path, "w", encoding="utf-8") as file:
+        file.write(final_encrypted)
+
+    return "RC4"
+
+def KSA(key):
+    key_length = len(key)
+    S = list(range(256))
+    j = 0
+    for i in range(256):
+        j = (j + S[i] + key[i % key_length]) % 256
+        S[i], S[j] = S[j], S[i]
+
+    return S
+
+def PRGA(S, n):
+    i = 0
+    j = 0
+    key = []
+
+    while n>0:
+        n = n-1
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        S[i], S[j] = S[j], S[i]
+        K = S[(S[i] + S[j]) % 256]
+        key.append(K)
+    return key
+
+def preparing_key_array(s):
+    return [ord(c) for c in s]
 
 def read_config(path):
     with open(path, "r") as stream:
@@ -86,7 +134,10 @@ if __name__ == "__main__":
 
         filepath = f"{cfg.ABSOLUTEPATH}/client/static/{filename}"
 
-        iv = encrypt(filepath, key, mode)
+        if(mode != 7):
+            iv = encrypt(filepath, key, mode)
+        else:
+            iv = encryptRC4(filepath, key)
 
         realname = f"{cfg.ABSOLUTEPATH}/client/encrypted/{filename}"
         enc_path = realname.replace('.txt', '.bin')
